b379866249a26c085af586daf57020f916d8b1d2
anthonypileggi <anthony@ifixit.com>
2019-02-15 13:40:46 -0500

    update 'sa_api' to iterate over all pages in the response

--- R/sa_api.R
+++ R/sa_api.R
@@ -1,26 +1,40 @@
 #' Make a SellerActive API call
+#' @export
+sa_api <- function(...) {
+  pg <- 1
+  x <- NULL
+  while (pg == 1 || length(x_new) > 0) {
+    message("Getting page ", pg, " of API data.")
+    x_new <- sa_api_call(..., req.page = pg, req.size = 100)
+    x <- c(x, x_new)
+    pg <- pg + 1
+  }
+  return(x)
+}
+
+#' Make a SellerActive API call
 #' @param endpoint api endpoint
 #' @param ... additional named parameters
 #' @export
-sa_api <- function(endpoint = NULL) {
-  
+sa_api_call <- function(endpoint = NULL, ...) {
+
   if (is.null(endpoint))
     stop("You must specify an endpoint for the SellerActive API!", call. = FALSE)
-  
+
   # call api
-  url <- 
+  url <-
     httr::modify_url(
-      url = "https://rest.selleractive.com:443", 
+      url = "https://rest.selleractive.com:443",
       path = paste0("api/", endpoint),
-      query = list(req.sKU = "49065-7007")
+      query = list(...)
     )
-  response <- 
+  response <-
     httr::GET(
-      url, 
+      url,
       httr::add_headers(Authorization = paste("Basic", Sys.getenv("SELLERACTIVE_AUTH_KEY"))),
       encode = "json"
     )
-  
+
   # check status code
   if (httr::status_code(response) != 200) {
     stop(
@@ -31,7 +45,7 @@ sa_api <- function(endpoint = NULL) {
       call. = FALSE
     )
   }
-  
+
   #response
   httr::content(response)
 }
